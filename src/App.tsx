469fc79c1195c11482d09234dc8f22d1dfc2b895
Gabriel Barbosa <59917720+gabrielb-ss@users.noreply.github.com>
2023-01-07 16:24:12 -0300

    chore: add global style

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
+import styled from 'styled-components'
+import { GlobalStyle } from './styles/global';
+
 export function App() {
   return (
     <div className="App">
        <h1>Hello World</h1>
+       <GlobalStyle></GlobalStyle>
     </div>
   );
 }
